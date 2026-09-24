d5b1bc948261a47c0fe47f465ab325e234a60724
Yamilet <yamiletmari724@gmail.com>
2026-09-23 22:03:47 -0600

    feat: conectar frontend con API publicada

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:3001/api/estudiantes';
+const API_URL =
+    'https://api-maestro-detalle-yamilet.onrender.com/api/estudiantes';
 
 async function cargarEstudiantes() {
 
